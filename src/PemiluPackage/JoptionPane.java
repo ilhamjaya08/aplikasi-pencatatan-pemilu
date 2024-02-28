1e0c04128cc2d06b506f4a2c87f2e07b56aebb08
ilhamjaya08 <m.ilhamjaya1808@gmail.com>
2024-02-28 12:57:47 +0700

    Add Code

---
+++ src/PemiluPackage/JoptionPane.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PemiluPackage;
+
+/**
+ *
+ * @author RPL 3_24
+ */
+class JoptionPane {
+        public static void showMessageDialog(Object object, Exception ex){
+             throw new UnsupportedOperationException("Not Supported Yet");
+        }
+        public JoptionPane(){
+            
+        }
+    }
